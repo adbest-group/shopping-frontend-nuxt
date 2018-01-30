7386f238ba61a2de886d014b5070d4e00f87e26b
lmh <495453865@qq.com>
2018-01-30 18:21:03 +0800

    http换成https

--- utils/Global.js
+++ utils/Global.js
@@ -8,7 +8,7 @@ export default
 // api 地址
 // export const apiUrl ='http://10.2.30.54:9000/api';
 // export const apiUrl ='http://127.0.0.1:8085';
-export const apiUrl = 'http://server.dealswill.com/api'
+export const apiUrl = 'https://server.dealswill.com/api'
 
 // 后台自动获取 新增条数 的 时间间隔  单位 毫秒
 export const autoFetchTime = 30000
